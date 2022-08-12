d42255a8a6bd4a80c4c8f0b5cb24eb029b57bc4c
Christopher Paciorek <paciorek@stat.berkeley.edu>
2022-08-12 11:42:51 -0700

    update site config

--- publish.sh
+++ publish.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-git checkout gh-pages && cp -pr _site/* . && git commit -am'update website' && git push && git checkout main
+git checkout gh-pages && cp -pr _site/* . && git commit -am'update website' && git push
+git checkout main
