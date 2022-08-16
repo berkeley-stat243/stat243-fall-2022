e39ac959205f30579b029d0927006aeb6ef5e696
Christopher Paciorek <paciorek@stat.berkeley.edu>
2022-08-16 11:57:13 -0700

    update publish.sh

--- publish.sh
+++ publish.sh
@@ -6,6 +6,7 @@
 ## Step 2: Render individual files to html with `quarto render file.ext`, which puts rendered files in _site. Don't commit anything in _site.
 ## Step 3: Commit source file changes to main branch.
 ## Step 4: If adding any new html files, copy the new html from _site to the root directory do git add and commit into gh-pages branch.
+## Step 4a: If adding anything to main nav, update `_quarto.yml`. Unfortunately, one also needs to run `quarto render` on all source files so that they get the updated nav.  
 ## Step 5: Publish as follows from root directory:
 git checkout gh-pages && cp -pr _site/* . && git commit -am'update website' && git push
 git checkout main
